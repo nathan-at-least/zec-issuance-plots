4e1864fac168cbd7d909e4ffe2a9449dff4a3d54
user <user@penguin>
2022-07-26 16:48:36 -0700

    Fix downsample and unit test expectations.

--- src/downsample.rs
+++ src/downsample.rs
@@ -1,45 +1,104 @@
 const THRESHOLD: f32 = 0.01;
 
 /// Downsample data sets by removing points where Y coordinates vary little.
-pub fn downsample<I, X>(pts: I) -> impl Iterator<Item = (X, f32)>
+
+pub type Point<X> = (X, f32);
+
+pub fn downsample<I, X>(pts: I) -> impl Iterator<Item = Point<X>>
 where
-    I: Iterator<Item = (X, f32)>,
+    I: Iterator<Item = Point<X>>,
+    X: std::fmt::Debug,
 {
-    Downsampler { pts, state: None }
+    Downsampler {
+        pts: Some(pts),
+        latch: None,
+        yieldlatch: None,
+    }
 }
 
-struct Downsampler<I, X>
-where
-    I: Iterator<Item = (X, f32)>,
-{
-    pts: I,
-    state: Option<(f32, (X, f32))>,
+struct Downsampler<I, X> {
+    pts: Option<I>,
+    latch: Option<Latch<X>>,
+    yieldlatch: Option<Latch<X>>,
+}
+
+#[derive(Debug)]
+enum Latch<X> {
+    Singleton(Point<X>),
+    Span(Point<X>, Point<X>),
 }
 
 impl<I, X> Iterator for Downsampler<I, X>
 where
-    I: Iterator<Item = (X, f32)>,
+    I: Iterator<Item = Point<X>>,
+    X: std::fmt::Debug,
 {
-    type Item = (X, f32);
+    type Item = Point<X>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(pt) = self.pts.next() {
-            let y = pt.1;
+        use Latch::*;
 
-            if let Some((latch, prevpt)) = self.state.take() {
-                if (latch - y) / latch >= THRESHOLD {
-                    self.state = Some((y, pt));
-                    Some(prevpt)
-                } else {
-                    self.state = Some((latch, pt));
-                    None
+        if let Some(yl) = self.yieldlatch.take() {
+            match yl {
+                Singleton(pt) => Some(pt),
+                Span(a, b) => {
+                    self.yieldlatch = Some(Singleton(b));
+                    Some(a)
                 }
-            } else {
-                self.state = Some((y, pt));
-                None
             }
+        } else if let Some(pts) = self.pts.as_mut() {
+            for pt in pts {
+                if let Some(latch) = self.latch.take() {
+                    if latch.past_threshold(pt.1) {
+                        self.yieldlatch = Some(latch);
+                        self.latch = Some(Singleton(pt));
+                        return self.next();
+                    } else {
+                        self.latch = Some(latch.update(pt))
+                    }
+                } else {
+                    self.latch = Some(Singleton(pt));
+                }
+            }
+
+            self.pts = None;
+            self.yieldlatch = self.latch.take();
+            self.next()
         } else {
-            self.state.take().map(|(_, lastpt)| lastpt)
+            None
+        }
+    }
+}
+
+impl<X> Latch<X> {
+    fn past_threshold(&self, y: f32) -> bool {
+        let latchval = self.latch_val();
+        let delta = (latchval - y).abs();
+        let thresh = if latchval < THRESHOLD / 1e6 {
+            // Avoid divide by 0:
+            delta
+        } else {
+            delta / latchval
+        };
+
+        thresh >= THRESHOLD
+    }
+
+    fn latch_val(&self) -> f32 {
+        use Latch::*;
+
+        match self {
+            Singleton((_, y)) => *y,
+            Span((_, y), _) => *y,
+        }
+    }
+
+    fn update(self, pt: Point<X>) -> Self {
+        use Latch::*;
+
+        match self {
+            Singleton(a) => Span(a, pt),
+            Span(a, _) => Span(a, pt),
         }
     }
 }
--- src/downsample/tests.rs
+++ src/downsample/tests.rs
@@ -1,7 +1,74 @@
+use crate::downsample::downsample;
 use test_case::test_case;
 
-#[test_case([] => Vec::<(usize, f32)>::new())]
-#[test_case([(0, 0.0)] => vec![(0, 0.0)])]
-fn downsample<const K: usize>(inputs: [(usize, f32); K]) -> Vec<(usize, f32)> {
-    crate::downsample::downsample(inputs.into_iter()).collect()
+#[test_case([] => Vec::<f32>::new())]
+#[test_case(
+    [
+        0.0,
+    ] => vec![
+        0.0,
+    ]
+)]
+#[test_case(
+    [
+        0.0,
+        0.0,
+    ] => vec![
+        0.0,
+        0.0,
+    ]
+)]
+#[test_case(
+    [
+        0.0,
+        0.002,
+        0.0,
+    ] => vec![
+        0.0,
+        0.0,
+    ]
+)]
+#[test_case(
+    [
+        0.0,
+        7.0,
+        0.0,
+    ] => vec![
+        0.0,
+        7.0,
+        0.0,
+    ]
+)]
+#[test_case(
+    [
+        0.0,
+        7.0,
+        7.002,
+        0.0,
+    ] => vec![
+        0.0,
+        7.0,
+        7.002,
+        0.0,
+    ]
+)]
+#[test_case(
+    [
+        0.0,
+        7.0,
+        7.002,
+        7.001,
+        42.0,
+    ] => vec![
+        0.0,
+        7.0,
+        7.001,
+        42.0,
+    ]
+)]
+fn test_downsample<const K: usize>(inputs: [f32; K]) -> Vec<f32> {
+    dbg!(&inputs);
+    downsample(inputs.into_iter().enumerate())
+        .map(|(_, y)| y)
+        .collect()
 }
