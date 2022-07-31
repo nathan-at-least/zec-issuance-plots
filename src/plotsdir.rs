d31146d3995e0f9d696146838ec14298ec8e6f93
user <user@penguin>
2022-07-30 17:14:57 -0700

    Remove flaky `testdir` crate which lead to nondeterministic pass/fail.

--- src/plotsdir.rs
+++ src/plotsdir.rs
@@ -1,61 +1,77 @@
 use crate::PLOTS_DIR;
+use std::path::Path;
 
 pub fn refresh() -> std::io::Result<()> {
-    std::fs::remove_dir_all(PLOTS_DIR).or_else(|err| {
+    refresh_dir(PLOTS_DIR)
+}
+
+fn refresh_dir<P>(dir: P) -> std::io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let path = dir.as_ref();
+    remove_dir_if_present(path)?;
+    std::fs::create_dir_all(path)?;
+    Ok(())
+}
+
+fn remove_dir_if_present<P>(dir: P) -> std::io::Result<()>
+where
+    P: AsRef<Path>,
+{
+    std::fs::remove_dir_all(dir).or_else(|err| {
         use std::io::ErrorKind::NotFound;
         match err.kind() {
             // It's ok if it didn't exist:
             NotFound => Ok(()),
 
             // Propagate other errors:
-            other => Err(other),
+            _ => Err(err),
         }
-    })?;
-
-    std::fs::create_dir_all(PLOTS_DIR)?;
-
-    Ok(())
+    })
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{plotsdir, PLOTS_DIR};
-    use testdir::testdir;
+    use super::refresh_dir;
+    use std::path::PathBuf;
+
+    fn prep_test_plots_dir(testname: &str) -> std::io::Result<PathBuf> {
+        let pb = PathBuf::from("target")
+            .join("test")
+            .join(module_path!())
+            .join(testname);
+        refresh_dir(&pb)?;
+        Ok(pb.join("plots"))
+    }
 
     #[test]
     fn new_dir() -> std::io::Result<()> {
-        let d = testdir!();
-        std::env::set_current_dir(&d)?;
-        plotsdir::refresh()?;
-        assert!(d.join(PLOTS_DIR).is_dir());
+        let plotsdir = prep_test_plots_dir("new_dir")?;
+        refresh_dir(&plotsdir)?;
+        assert!(plotsdir.is_dir());
         Ok(())
     }
 
     #[test]
     fn existing_dir() -> std::io::Result<()> {
-        let d = testdir!();
-        std::env::set_current_dir(&d)?;
+        let plotsdir = prep_test_plots_dir("existing_dir")?;
+        std::fs::create_dir(&plotsdir)?;
 
-        let plots = d.join(PLOTS_DIR);
-        dbg!(&plots);
-        std::fs::create_dir(&plots)?;
+        refresh_dir(&plotsdir)?;
 
-        plotsdir::refresh()?;
-
-        assert!(plots.is_dir());
+        assert!(plotsdir.is_dir());
         Ok(())
     }
 
     #[test]
     fn propagated_error() -> std::io::Result<()> {
-        let d = testdir!();
-        std::env::set_current_dir(&d)?;
+        let plotsdir = prep_test_plots_dir("propagated_error")?;
 
-        let plots = d.join(PLOTS_DIR);
         // Write a file, which should cause rmdir to fail:
-        std::fs::write(&plots, &[])?;
+        std::fs::write(&plotsdir, &[])?;
 
-        let r = plotsdir::refresh();
+        let r = refresh_dir(plotsdir);
 
         assert!(r.is_err());
         Ok(())
