c37f0b3365cd68c7f1949a8929c25c37e0d4bb5d
user <user@penguin>
2022-08-01 18:36:11 -0700

    Simplify the zpf issuance to one comparison; re-enable other charts.

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let h = halving_height(5);
         h + (h / 10)
     };
-    let (zpf_activation_height_a, zpf_activation_height_b) = {
+    let zpf_activation_height = {
         let b = POST_BLOSSOM_HALVING_INTERVAL;
         let h = halving_height(2);
-        (h - (b / 5), h - (b / 7))
+        h + (b / 4)
     };
 
     let zectime = TimeModel::new(Chain::Zcash);
@@ -46,40 +46,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         btc_max_height
     );
 
-    if false {
-        LinePlot {
-            file_stem: "issuance-nu5",
-            caption: "ZEC Issuance per 10m Interval (as of NU5 protocol)",
-            datasets: vec![gen_issuance_dataset(NU5, zec_max_height)],
-            points: false,
-        }
-        .plot()?;
-
-        LinePlot {
-            file_stem: "supply-btc-vs-nu5",
-            caption: "Supplies of BTC & ZEC (as of NU5 protocol)",
-            datasets: vec![
-                DataSet::new("supply cap".to_string(), {
-                    vec![
-                        (btctime.at(0), max_supply),
-                        (btctime.at(btc_max_height), max_supply),
-                    ]
-                }),
-                gen_supply_dataset(zectime, NU5, zec_max_height),
-                gen_supply_dataset(btctime, Btc, btc_max_height),
-            ],
-            points: false,
-        }
-        .plot()?;
+    LinePlot {
+        file_stem: "issuance-nu5",
+        caption: "ZEC Issuance per 10m Interval (as of NU5 protocol)",
+        datasets: vec![gen_issuance_dataset(NU5, zec_max_height)],
+        points: false,
     }
+    .plot()?;
+
+    LinePlot {
+        file_stem: "supply-btc-vs-nu5",
+        caption: "Supplies of BTC & ZEC (as of NU5 protocol)",
+        datasets: vec![
+            DataSet::new("supply cap".to_string(), {
+                vec![
+                    (btctime.at(0), max_supply),
+                    (btctime.at(btc_max_height), max_supply),
+                ]
+            }),
+            gen_supply_dataset(zectime, NU5, zec_max_height),
+            gen_supply_dataset(btctime, Btc, btc_max_height),
+        ],
+        points: false,
+    }
+    .plot()?;
 
     LinePlot {
         file_stem: "issuance-zpf",
         caption: "ZPF Issuance per 10m Interval",
         datasets: vec![
             gen_issuance_dataset(NU5, zec_max_height),
-            gen_issuance_dataset(PosterityFund(zpf_activation_height_a), zec_max_height),
-            gen_issuance_dataset(PosterityFund(zpf_activation_height_b), zec_max_height),
+            gen_issuance_dataset(PosterityFund(zpf_activation_height), zec_max_height),
+        ],
+        points: false,
+    }
+    .plot()?;
+
+    LinePlot {
+        file_stem: "supply-zpf",
+        caption: "Supply of ZEC",
+        datasets: vec![
+            DataSet::new("supply cap".to_string(), {
+                vec![
+                    (zectime.at(0), max_supply),
+                    (zectime.at(zec_max_height), max_supply),
+                ]
+            }),
+            gen_supply_dataset(zectime, NU5, zec_max_height),
+            gen_supply_dataset(
+                zectime,
+                PosterityFund(zpf_activation_height),
+                zec_max_height,
+            ),
         ],
         points: false,
     }
